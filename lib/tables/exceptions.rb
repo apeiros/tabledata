3a828f1ee4a8813f16f7e673e906a52f8d79cf9c
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-06-22 13:40:31 +0200

    Fixed a bug. Exceptions were not intended to be nested within Tables::Exception.

--- lib/tables/exceptions.rb
+++ lib/tables/exceptions.rb
@@ -2,16 +2,16 @@
 
 module Tables
   module Exception
-    class InvalidFileType < ArgumentError
-      include Exception
-    end
-    class InvalidColumnCount < ArgumentError
-      include Exception
-    end
-    class InvalidColumnSpecifier < ArgumentError
-      include Exception
-    end
-    class InvalidColumnName < InvalidColumnSpecifier; end
-    class InvalidColumnAccessor < InvalidColumnSpecifier; end
   end
+  class InvalidFileType < ArgumentError
+    include Exception
+  end
+  class InvalidColumnCount < ArgumentError
+    include Exception
+  end
+  class InvalidColumnSpecifier < ArgumentError
+    include Exception
+  end
+  class InvalidColumnName < InvalidColumnSpecifier; end
+  class InvalidColumnAccessor < InvalidColumnSpecifier; end
 end
