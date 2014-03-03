47ecb340afe70f1e451923a70b3bd9cc62e714aa
popl <popl@cpan.org>
2014-03-02 22:38:05 -0800

    Created and added skeleton test files for Column and Row.

---
+++ test/unit/lib/tabledata/column.rb
@@ -0,0 +1,35 @@
+suite "TableData::Column" do
+    test 'Column#table' do
+    end
+
+    test 'Column#index' do
+    end
+
+    test 'Column#header' do
+    end
+
+    test 'Column#accessor' do
+    end
+
+    test 'Column#[]' do
+    end
+
+    test 'Column#each' do
+    end
+
+    test 'Column#each_value' do
+    end
+
+    test 'Column#to_a' do
+    end
+
+    test 'Column#==' do
+    end
+
+    test 'Column#eql' do
+    end
+
+    test 'Column#hash' do
+    end
+
+end
---
+++ test/unit/lib/tabledata/row.rb
@@ -0,0 +1,52 @@
+suite "TableData#Row" do
+    test 'Row#table' do
+    end
+
+    test 'Row#index' do
+    end
+
+    test 'Row#data' do
+    end
+
+    test 'Row#each' do
+    end
+
+    test 'Row#[]' do
+    end
+
+    test 'Row#slice' do
+    end
+
+    test 'Row#at' do
+    end
+
+    test 'Row#at_header' do
+    end
+
+    test 'Row#at_accessor' do
+    end
+
+    test 'Row#at_index' do
+    end
+
+    test 'Row#values_at' do
+    end
+
+    test 'Row#size' do
+    end
+
+    test 'Row#to_hash' do
+    end
+
+    test 'Row#to_a' do
+    end
+
+    test 'Row#respond_to_missing' do
+    end
+
+    test 'Row#method_missing' do
+    end
+
+    test 'Row#inspect' do
+    end
+end
